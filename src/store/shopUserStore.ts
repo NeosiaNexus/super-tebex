eb51d34ecf04285e10c8f80e49be5c980e2f0144
NeosiaNexus <devneosianexus@gmail.com>
2025-07-05 01:04:56 +0200

    feat(shopUserStore): Add test & optimization

--- src/store/shopUserStore.ts
+++ src/store/shopUserStore.ts
@@ -1,22 +1,24 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { persist, subscribeWithSelector } from 'zustand/middleware';
 
-interface ShopUserStore {
+export interface ShopUserStore {
   username: string;
   setUsername: (username: string) => void;
   clearUsername: () => void;
 }
 
 const shopUserStore = create<ShopUserStore>()(
-  persist(
-    set => ({
-      username: '',
-      setUsername: (username: string) => set({ username }),
-      clearUsername: () => set({ username: '' }),
-    }),
-    {
-      name: 'shop-user-store',
-    },
+  subscribeWithSelector(
+    persist(
+      set => ({
+        username: '',
+        setUsername: (username: string) => set({ username }),
+        clearUsername: () => set({ username: '' }),
+      }),
+      {
+        name: 'shop-user-store',
+      },
+    ),
   ),
 );
 
