6dd1dcc3b0aa9f680bb2b050cd1e9de58b2231c1
NeosiaNexus <devneosianexus@gmail.com>
2025-11-18 01:29:08 +0100

    fix: Export

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,1 +1,6 @@
+export { default as initShopUrls } from './initShopUrls';
+export { default as initTebex } from './initTebex';
 export { default as getTebex } from './tebexClient';
+
+export * from './initShopUrls';
+export * from './initTebex';
